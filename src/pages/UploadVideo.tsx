c9b66e73d83d6c292c94f42c9c2156283dfd51b1
shl_jessie <jessielee.shl@gmail.com>
2020-11-23 20:25:54 +0900

    upload videos section template updates

--- src/pages/UploadVideo.tsx
+++ src/pages/UploadVideo.tsx
@@ -1,76 +1,60 @@
-import { DefaultInnerRootCont, DefaultOuterRootCont } from "../components/Containers"
+import { DefaultInnerRootCont, DefaultOuterRootCont }  from '../components/Containers'
+import Dropzone, { IDropzoneProps, ILayoutProps } from 'react-dropzone-uploader'
+import { HEADER_HEIGHT, MAX_WIDTH } from "../constants/GlobalStyles"
 
-import FilledButton from '../components/Buttons'
-import Layout from "../components/layout"
-import Link from '@material-ui/core/Link';
-import React from "react"
-import { Link as RouterLink } from 'react-router-dom'
-import { Typography } from "@material-ui/core"
+import FilledButton from "../components/Buttons"
+import React from 'react'
 import UserHeader from '../layout/UserHeader'
 import { makeStyles } from "@material-ui/core/styles"
-import styled from "styled-components"
 import theme from "../theme"
 
-const ReadyCont = styled.div`
-    width: 100%; 
-    height:50vh;
-    margin-bottom: 0%;
-    background-color: #8687FF;
-`
-
 const useStyles = makeStyles(theme => ({
-  link: {
-    textAlign: 'center',
-    color: '#fff',
+  innerRoot :{
+    display:'flex',
+    justifyContent: 'center'
   },
-  readyInnerRoot:{
-    display: 'block',
-    marginLeft: 'auto',
-    paddingTop: theme.spacing(15),
-    marginRight: 'auto',
-    width: '50%',
-  },
-  readyTitle: {
-    fontWeight: 'bold',
-    color: '#fff',
-    marginBottom: theme.spacing(3)
-  },
-  button:{
-    backgroundColor: 'black',
-    width:'180px',
-    height: '41px',
-    border:"none",
-    borderRadius: '0px',
-    '&:hover': {
-      backgroundColor: '#fff',
-      boxShadow: `0 0 14px #fff`,
-      color: 'black'
-    },
-  },
-  descrip: {
-    marginTop: theme.spacing(2),
-    color: '#fff'
-  },
+  uploadbox: {
+    margin: 'auto',
+    justifyContent: 'center'
+  }
 }))
 
-const PageTemplate = (props: Props) => {
-  const {} = props
+const Layout = ({ input, previews, submitButton, dropzoneProps, files, extra: { maxFiles } }: ILayoutProps) => {
 
   const classes = useStyles()
-
   return (
-    <Layout>
-      <UserHeader/>
-      <DefaultOuterRootCont>
-          <DefaultInnerRootCont className={classes.readyInnerRoot}>
-
-        
-          </DefaultInnerRootCont>
-      </DefaultOuterRootCont>
-    </Layout>
+    <div className={classes.uploadbox}>
+      {previews}
+      <div {...dropzoneProps}>{files.length < maxFiles && input}</div>
+      {files.length > 0 && submitButton}
+    </div>
   )
 }
 
-type Props = {}
+const UploadVideo = () => {
+  const classes = useStyles()
+  const getUploadParams: IDropzoneProps['getUploadParams'] = () => ({ url: 'https://httpbin.org/post' })
 
-export default PageTemplate
+  const handleSubmit: IDropzoneProps['onSubmit'] = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  }
+
+  return (
+    <>
+    <UserHeader />
+    <DefaultOuterRootCont>
+      <DefaultInnerRootCont className={classes.innerRoot}>
+        <Dropzone
+          getUploadParams={getUploadParams}
+          LayoutComponent={Layout}
+          onSubmit={handleSubmit}
+          inputContent="Drop Files (Custom Layout)"
+        />
+      </DefaultInnerRootCont>
+    </DefaultOuterRootCont>
+    </>
+  )
+}
+
+export default UploadVideo;
