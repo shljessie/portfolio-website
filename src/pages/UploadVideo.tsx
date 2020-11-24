4f4b6bae44318c084c39f7b2a27d3abc92098b21
shl_jessie <jessielee.shl@gmail.com>
2020-11-24 13:47:03 +0900

    file uploader completed

--- src/pages/UploadVideo.tsx
+++ src/pages/UploadVideo.tsx
@@ -1,56 +1,124 @@
 import { DefaultInnerRootCont, DefaultOuterRootCont }  from '../components/Containers'
-import Dropzone, { IDropzoneProps, ILayoutProps } from 'react-dropzone-uploader'
-import { HEADER_HEIGHT, MAX_WIDTH } from "../constants/GlobalStyles"
+import { makeStyles, withStyles } from "@material-ui/core/styles"
 
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import FilledButton from "../components/Buttons"
+import {InputCont} from "../components/InputCont";
+import Link from '@material-ui/core/Link';
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import UserHeader from '../layout/UserHeader'
-import { makeStyles } from "@material-ui/core/styles"
+import VideoCallIcon from '@material-ui/icons/VideoCall';
 import theme from "../theme"
+
+const UrlCont = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'white',
+      },
+      '&:hover fieldset': {
+        borderColor: 'white',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'white',
+      },
+    },
+  },
+})(TextField);
 
 const useStyles = makeStyles(theme => ({
   innerRoot :{
     display:'flex',
-    justifyContent: 'center'
+    alignItems: 'center',
+    flexDirection: 'column'
   },
   uploadbox: {
     margin: 'auto',
     justifyContent: 'center'
+  },
+  box:{
+    width: '700px',
+    height: '400px',
+    marginTop: '10%',
+    border: '1px solid white',
+    display:'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  input: {
+    display: 'none',
+  },
+  link: {
+    marginTop: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    color: '#8687FF',
+    textDecoration: 'none'
+  },
+  urlCont: {
+    margin: theme.spacing(3),
+    width: '700px',
+    color: 'white'
+  },
+  inputText:{
+    color: "white"
   }
 }))
 
-const Layout = ({ input, previews, submitButton, dropzoneProps, files, extra: { maxFiles } }: ILayoutProps) => {
-
-  const classes = useStyles()
-  return (
-    <div className={classes.uploadbox}>
-      {previews}
-      <div {...dropzoneProps}>{files.length < maxFiles && input}</div>
-      {files.length > 0 && submitButton}
-    </div>
-  )
-}
-
 const UploadVideo = () => {
-  const classes = useStyles()
-  const getUploadParams: IDropzoneProps['getUploadParams'] = () => ({ url: 'https://httpbin.org/post' })
 
-  const handleSubmit: IDropzoneProps['onSubmit'] = (files, allFiles) => {
-    console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
+  const handleChange = (event: any) => {
+    
   }
 
+  const classes = useStyles()
   return (
     <>
     <UserHeader />
     <DefaultOuterRootCont>
       <DefaultInnerRootCont className={classes.innerRoot}>
-        <Dropzone
-          getUploadParams={getUploadParams}
-          LayoutComponent={Layout}
-          onSubmit={handleSubmit}
-          inputContent="Drop Files (Custom Layout)"
-        />
+      <Box color="white" className={classes.box}>
+        <VideoCallIcon style={{ fontSize: "90" }} />
+          <Typography variant="body1">
+          Upload an audio or video file. <br/>
+          Click to add media or drag and drop a file
+          </Typography>
+          <input
+              accept="video/*,audio/*"
+              className={classes.input}
+              id="contained-button-file"
+              multiple
+              type="file"
+            />
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" color="primary" component="span" style={{marginTop: theme.spacing(3)}}>
+                Add media
+              </Button>
+            </label>
+      </Box>
+      <UrlCont
+        className={classes.urlCont}
+        label=""
+        variant="outlined"
+        id="custom-css-outlined-input"
+        placeholder="Paste video or music URL (e.g. https://www.youtube.com/watch?v=oWhkL4899u8)"
+        InputProps={{
+          className: classes.inputText
+        }}
+      />
+      <Link component={RouterLink} to="resetpassword" className={classes.link}> 
+        Or Try a Sample
+      </Link>
       </DefaultInnerRootCont>
     </DefaultOuterRootCont>
     </>
